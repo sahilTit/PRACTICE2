b7ab0c4efa7d00d44d6864a4869b0b1e74add5fc
sahilTit <titarmaresahil2003@gmail.com>
2024-01-16 21:46:48 +0530

    16-01-24

--- Calculator/src/App.jsx
+++ Calculator/src/App.jsx
@@ -1,10 +1,13 @@
 import styles from './App.module.css'
-
+import Display from './components/Display'
 function App() {
   return <div className={styles.calculator}>
-    <input className={styles.display} type="text" />
-    <div id="buttons-container">
-      <button>C</button>
+    <Display></Display>
+    <div className={styles.buttonsContainer}>
+      <button className={styles.button}>C</button>
+      <button className={styles.button}>1</button>
+      <button className={styles.button}>2</button>
+      <button className={styles.button}>+</button>
     </div>
   </div>
 }
