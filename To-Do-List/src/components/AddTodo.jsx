590357690b515afa955a716eabaadb03e4b3279b
sahilTit <titarmaresahil2003@gmail.com>
2024-01-23 22:20:22 +0530

    23-01-2024

--- To-Do-List/src/components/AddTodo.jsx
+++ To-Do-List/src/components/AddTodo.jsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
+
 function AddTodo({ onNewItem }) {
+
+    const [todoName, setTodoName] = useState();
+    const [dueDate, setDueDate] = useState();
+
+    const handleNameChange = (event) => {
+        setTodoName(event.target.value);
+    }
+    const handleDateChange = (event) => {
+        setDueDate(event.target.value);
+    }
+    const handleAddButtonClicked = () => {
+        onNewItem(todoName, dueDate);
+        setTodoName("");
+        setDueDate("");
+    }
     return <div className="container items-container">
         <div className="row">
             <div className="col-6">
-                <input type="text" placeholder="Enter Todo Here" /></div>
+                <input type="text" placeholder="Enter Todo Here" value={todoName} onChange={handleNameChange} /></div>
             <div className="col-4">
-                <input type="date" name="" id="" /></div>
+                <input type="date" value={dueDate} onChange={handleDateChange} /></div>
             <div className="col-2">
                 <button className="btn btn-success st-button"
-                    onClick={() => { onNewItem('a', 'b') }}
+                    onClick={handleAddButtonClicked}
                 >Add</button></div>
         </div>
     </div >
