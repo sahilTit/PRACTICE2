4785bbb1b37d74d1752ba7bc62c32444398d6fd1
sahilTit <titarmaresahil2003@gmail.com>
2024-01-24 16:22:55 +0530

    24-01-2024

--- To-Do-List/src/App.jsx
+++ To-Do-List/src/App.jsx
@@ -1,28 +1,22 @@
 import AppName from "./components/AppName"
 import AddTodo from "./components/AddTodo"
 import TodoItems from "./components/TodoItems"
+import WelcomeMessage from "./components/WelcomeMessage"
 import "./App.css"
 import { useState } from "react";
 function App() {
-  const initialTodoItems = [{
-    name: "Buy Milk",
-    dueDate: "31/12/2023"
-  }, {
-    name: "Buy Coffee",
-    dueDate: "31/12/2023",
-  }, {
-    name: "Buy Tea",
-    dueDate: "31/12/2023",
-  }
-  ];
-  const [todoItems, setTodoItems] = useState(initialTodoItems)
+
+  const [todoItems, setTodoItems] = useState([]);
 
   const handleNewItem = (itemName, itemDueDate) => {
-    console.log(`New item added:${itemName} date:${itemDueDate}`)
+    console.log(`New item added:${itemName} date:${itemDueDate}`);
+    const newTodoItems = [...todoItems, { name: itemName, dueDate: itemDueDate }];
+    setTodoItems(newTodoItems);
   }
   return <center className='todo-container'>
     <AppName />
-    <AddTodo onNewItem={handleNewItem}/>
+    <AddTodo onNewItem={handleNewItem} />
+    {todoItems.length === 0 && <WelcomeMessage></WelcomeMessage>}
     <TodoItems TodoItems={todoItems}></TodoItems>
   </center>
 }
