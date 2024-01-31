a0a7e827337a06dbb6c973da292788396d901cf8
sahilTit <titarmaresahil2003@gmail.com>
2024-01-31 22:31:27 +0530

    31-01-2024

---
+++ Social-Media/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Sidebar from './components/Sidebar';
+
+function App() {
+  return <>
+    <Header></Header>
+    <Sidebar></Sidebar>
+    <Footer></Footer>
+  </>
+}
+
+export default App
