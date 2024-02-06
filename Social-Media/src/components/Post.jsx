2cb7fa1ddebf67c237c8b62bd276b85bdad93180
sahilTit <titarmaresahil2003@gmail.com>
2024-02-06 07:01:28 +0530

    06-02-2024

--- Social-Media/src/components/Post.jsx
+++ Social-Media/src/components/Post.jsx
@@ -1,17 +1,21 @@
+import { MdDelete } from "react-icons/md";
 const Post = ({ post }) => {
-  return <div className="card  post-card" style={{ width: "18rem" }}>
+  return <div className="card  post-card" style={{ width: "30rem" }}>
     <div className="card-body">
       <h5 className="card-title ">{post.title}
         <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-          99+
+          <MdDelete />
           <span className="visually-hidden">unread messages</span>
         </span>
 
       </h5>
       <p className="card-text">{post.body}</p>
       {post.tags.map((tag) => (
-        <span key={} className="badge text-bg-primary hashtag">#{tag}</span>
+        <span className="badge text-bg-primary hashtag">#{tag}</span>
       ))}
+      <div className="alert alert-success" role="alert">
+        A simple success alert—check it out!
+      </div>
     </div>
   </div>
 }
